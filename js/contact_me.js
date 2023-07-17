c1a5ee9592cbbb6b898dbd0509c301a43f3bd1e8
Thomas Brears <fcx9443@autuni.ac.nz>
2023-07-17 20:41:06 +1200

    Contact us section major update

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,8 +8,11 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
-      var firstName = WSA Resident; // For Success/Failure Message
+      var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
@@ -17,9 +20,12 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "contact_me.php",
+        url: "././mail/contact_me.php",
         type: "POST",
         data: {
+          name: name,
+          phone: phone,
+          email: email,
           message: message
         },
         cache: false,
@@ -29,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong> Thanks for your post/confession. It'll be posted to @WSAConfession23 soon</strong>");
+            .append("<strong>Your message has been sent! We will reply soon</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -40,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that our mail server is not responding. Please try again later or DM us"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later or email support@weboid.dev"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,13 +15,12 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 $from_ip = $_SERVER['REMOTE_ADDR'];
-$from_browser = $_SERVER['HTTP_USER_AGENT'];
    
 // Create the email and send the message
-$to = 'hello@weboid.dev'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "New email from $name || Weboid Contact From";
-$email_body = "New contact form message from $name \n\n Receved on " . date('d-m-Y') . "\n\n\n\n Message:\n ------------ \n Name: $name \n Email: $email_address \n Phone: $phone \n Message: $message \n\n\n\n $name sent this from the IP, " . $from_ip . " with the browser, " . $from_browser . " n\n\n\n Weboid Contact Form - www.weboid.dev - support@weboid.dev - 027 269 0900";
-$headers = "From: system@weboid.dev\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'support@weboid.dev';
+$email_subject = "New contact submission from $name";
+$email_body = "New contact form message from $name \n\n\n Message:\n ------------ \n Name: $name \n Email: $email_address \n Phone: $phone \n Message: $message \n\n\n\n $name sent this on " . date('d-m-Y') .", from the IP " . $from_ip . " n\n\n\n Weboid Contact Form - weboid.dev - support@weboid.dev - +6427 269 0900";
+$headers = "From: $email_address\n"; 
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
